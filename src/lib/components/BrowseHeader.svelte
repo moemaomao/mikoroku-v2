ed261c17fae9aba47b04fd6be305ac0303149870
moemaomao <moemaomao@gmail.com>
2026-09-18 15:52:15 +0700

    Error

--- src/lib/components/BrowseHeader.svelte
+++ src/lib/components/BrowseHeader.svelte
@@ -325,8 +325,7 @@ async function selectMulti() {
 										{/if}
 										{#if meta.isError || isBrokenSource(source.id)}
 											<span
-												class="rounded bg-amber-500 px-1.5 py-0.5 text-[9px] font-bold text-black"
-											>
+												class="rounded border border-red-500/50 bg-red-500/15 px-1.5 py-0.5 text-[10px] font-bold text-red-400">
 												ERROR
 											</span>
 										{/if}
